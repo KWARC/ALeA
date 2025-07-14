44cdf660ecd56faa29f2211b88c68f8d7904e412
Keerthan K <79328813+keerthnn@users.noreply.github.com>
2025-07-14 10:53:26 +0530

    Simplified create issue flow (#673)
    * simplified create issue flow
    
    * refactor
    
    * Update issueCreator.ts
    
    * Update issueCreator.ts
    
    * minor changes
    
    * moving IsGitLab to api from frontend
    
    * Update issueCreator.ts
    
    * Update create-issue.ts
    
    * Update issueCreator.ts

--- packages/report-a-problem/src/lib/issueCreator.ts
+++ packages/report-a-problem/src/lib/issueCreator.ts
@@ -6,10 +6,6 @@ import axios from 'axios';
 
 const THREE_BACKTICKS = '```';
 
-export enum IssueCategory {
-  CONTENT = 'CONTENT',
-  DISPLAY = 'DISPLAY',
-}
 export interface SelectionContext {
   fragmentUri: FTML.URI;
   fragmentKind: 'Section' | 'Paragraph' | 'Slide' | 'Problem'; // Keep alingned with FTML.FragmentKind
@@ -25,7 +21,7 @@ async function addSources(context: SelectionContext[]): Promise<SelectionContext
   );
 }
 
-async function createSectionHierarchy(context: SelectionContext[]) {
+function createSectionHierarchy(context: SelectionContext[]) {
   if (!context?.length) return '';
   let returnVal = '### The selected text was in the following section hierarchy:\n\n';
   if (context.length > 1) returnVal += '**_INNERMOST SECTION FIRST_**\n\n';
@@ -42,13 +38,13 @@ async function createSectionHierarchy(context: SelectionContext[]) {
   return returnVal;
 }
 
-async function createIssueBody(
+function createIssueBody(
   desc: string,
   selectedText: string,
   userName: string,
   context: SelectionContext[]
 ) {
-  const sectionHierarchy = await createSectionHierarchy(context);
+  const sectionHierarchy = createSectionHierarchy(context);
   const user = userName || 'a user';
 
   return `An issue was logged by "${user}" at the following url:
@@ -68,49 +64,30 @@ ${THREE_BACKTICKS}
 ${sectionHierarchy}`;
 }
 
-function isGitlabIssue(category: IssueCategory, context: SelectionContext[]) {
-  return category === IssueCategory.CONTENT && context?.length > 0;
-}
-
-function getNewIssueUrl(category: IssueCategory, projectId: string, context: SelectionContext[]) {
-  if (!isGitlabIssue(category, context)) return 'https://api.github.com/repos/slatex/ALeA/issues';
-  return `https://gl.mathhub.info/api/v4/projects/${encodeURIComponent(projectId)}/issues`;
-}
-
-async function createIssueData(
-  category: IssueCategory,
+function createIssueData(
   desc: string,
   selectedText: string,
   context: SelectionContext[],
   userName: string
 ) {
-  const body = await createIssueBody(desc, selectedText, userName, context);
-  return {
-    ...(isGitlabIssue(category, context)
-      ? { description: body }
-      : { body, labels: ['user-reported'] }),
-  };
+  const issueText = createIssueBody(desc, selectedText, userName, context);
+  return issueText;
 }
 
 export async function createNewIssue(
-  category: IssueCategory,
   desc: string,
   selectedText: string,
   context: SelectionContext[],
   userName: string
 ) {
   const withSourceContext = await addSources(context);
-  const { project } = extractProjectAndFilepath(withSourceContext[0]?.source);
-  const projectId = project || 'sTeX/meta-inf';
-  const data = await createIssueData(category, desc, selectedText, withSourceContext, userName);
+  const data = createIssueData(desc, selectedText, withSourceContext, userName);
 
   try {
-    const createNewIssueUrl = getNewIssueUrl(category, projectId, context);
     const response = await axios.post(
       '/api/create-issue',
       {
         data,
-        createNewIssueUrl,
         description: desc,
         selectedText,
         context: withSourceContext[0]?.source,
