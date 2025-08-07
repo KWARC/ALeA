903cb3c18d32d3e57d90b0120f239bfb0a979a3a
anshumanTiwari <anshumant111@gmail.com>
2025-08-08 01:48:01 +0530

    changes in get semesterinfo
    removed authorisation

--- packages/alea-frontend/pages/api/university-admin/semester-info/get-semester-info.ts
+++ packages/alea-frontend/pages/api/university-admin/semester-info/get-semester-info.ts
@@ -6,18 +6,18 @@ import { executeQuery, checkIfGetOrSetError } from '../../comment-utils';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfGetOrSetError(req, res)) return;
 
-  const userId = await getUserIdIfAuthorizedOrSetError(
-    req,
-    res,
-    ResourceName.UNIVERSITY_SEMESTER_DATA,
-    Action.MUTATE,
-    {
-      universityId: Array.isArray(req.query.universityId)
-        ? req.query.universityId[0]
-        : req.query.universityId,
-    }
-  );
-  if (!userId) return;
+  // const userId = await getUserIdIfAuthorizedOrSetError(
+  //   req,
+  //   res,
+  //   ResourceName.UNIVERSITY_SEMESTER_DATA,
+  //   Action.MUTATE,
+  //   {
+  //     universityId: Array.isArray(req.query.universityId)
+  //       ? req.query.universityId[0]
+  //       : req.query.universityId,
+  //   }
+  // );
+  // if (!userId) return;
 
   const { universityId, instanceId } = req.query;
 
