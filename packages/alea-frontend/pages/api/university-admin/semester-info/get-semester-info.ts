0c37c670d83f4c47af8828cfe9ddef54ede54189
anshumanTiwari <anshumant111@gmail.com>
2025-08-10 11:11:17 +0530

    latest changes as per the suggestions

--- packages/alea-frontend/pages/api/university-admin/semester-info/get-semester-info.ts
+++ packages/alea-frontend/pages/api/university-admin/semester-info/get-semester-info.ts
@@ -1,23 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Action, ResourceName } from '@stex-react/utils';
-import { getUserIdIfAuthorizedOrSetError } from '../../access-control/resource-utils';
 import { executeQuery, checkIfGetOrSetError } from '../../comment-utils';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!checkIfGetOrSetError(req, res)) return;
-
-  // const userId = await getUserIdIfAuthorizedOrSetError(
-  //   req,
-  //   res,
-  //   ResourceName.UNIVERSITY_SEMESTER_DATA,
-  //   Action.MUTATE,
-  //   {
-  //     universityId: Array.isArray(req.query.universityId)
-  //       ? req.query.universityId[0]
-  //       : req.query.universityId,
-  //   }
-  // );
-  // if (!userId) return;
 
   const { universityId, instanceId } = req.query;
 
