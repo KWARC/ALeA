7663660e4c8bbb1c6af5a2da4fa4204226adf323
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2025-09-10 12:46:33 +0530

    old-quiz ui fix

--- packages/alea-frontend/pages/exp/old-quiz.tsx
+++ packages/alea-frontend/pages/exp/old-quiz.tsx
@@ -15,9 +15,7 @@ import { useEffect, useState } from 'react';
 
 function extractCourseIds(files: string[], semester: string): Promise<string[]> {
   return Promise.all(
-    files.map(
-      (filename) => getOldQuizFile(semester, filename).catch(() => null) 
-    )
+    files.map((filename) => getOldQuizFile(semester, filename).catch(() => null))
   ).then((quizFiles) => {
     const courseIds = new Set<string>();
     quizFiles.forEach((qc) => {
@@ -59,15 +57,14 @@ function OldQuizPage() {
   useEffect(() => {
     if (selectedCourseId) {
       Promise.all(
-        files.map(
-          (filename) =>
-            getOldQuizFile(selectedSemester, filename)
-              .then((qc) => ({
-                filename,
-                courseId: qc.courseId,
-                quizStartTs: qc.quizStartTs,
-              }))
-              .catch(() => null) 
+        files.map((filename) =>
+          getOldQuizFile(selectedSemester, filename)
+            .then((qc) => ({
+              filename,
+              courseId: qc.courseId,
+              quizStartTs: qc.quizStartTs,
+            }))
+            .catch(() => null)
         )
       ).then((fileInfos) => {
         const filtered = fileInfos
@@ -87,95 +84,101 @@ function OldQuizPage() {
 
   return (
     <MainLayout title="Old Quiz | ALeA">
-      <Box sx={{ maxWidth: 600, mx: 'auto', mt: 4, p: 2 }}>
-        <Typography variant="h6" gutterBottom>
-          Semester
-        </Typography>
-        <Select
-          value={selectedSemester}
-          onChange={(e) => setSelectedSemester(e.target.value)}
-          displayEmpty
-          fullWidth
-          sx={{ mb: 2 }}
-        >
-          <MenuItem value="" disabled>
-            Select semester
-          </MenuItem>
-          {semesters.map((s) => (
-            <MenuItem key={s} value={s}>
-              {s}
-            </MenuItem>
-          ))}
-        </Select>
-
-        {selectedSemester && (
-          <>
+      <Box sx={{ maxWidth: 1000, mx: 'auto', mt: 4, p: 2 }}>
+        <Box sx={{ display: 'flex', gap: 3 }}>
+          <Box sx={{ width: 320 }}>
             <Typography variant="h6" gutterBottom>
-              Course IDs
+              Semester
             </Typography>
-            <List>
-              {courseIds.map((cid) => (
-                <ListItem key={cid} disablePadding>
-                  <Button
-                    variant={selectedCourseId === cid ? 'contained' : 'outlined'}
-                    onClick={() => setSelectedCourseId(cid)}
-                    fullWidth
-                  >
-                    {cid}
-                  </Button>
-                </ListItem>
+            <Select
+              value={selectedSemester}
+              onChange={(e) => setSelectedSemester(e.target.value)}
+              displayEmpty
+              fullWidth
+              sx={{ mb: 2 }}
+            >
+              <MenuItem value="" disabled>
+                Select semester
+              </MenuItem>
+              {semesters.map((s) => (
+                <MenuItem key={s} value={s}>
+                  {s}
+                </MenuItem>
               ))}
-            </List>
-          </>
-        )}
+            </Select>
 
-        {selectedCourseId && (
-          <>
-            <Typography variant="h6" gutterBottom>
-              Quiz Files for <b>{selectedCourseId}</b>
-            </Typography>
-            <List>
-              {[...courseFileInfos]
-                .sort((a, b) => {
-                  return b?.quizStartTs - a.quizStartTs;
-                })
-                .map(({ filename, quizStartTs }) => (
-                  <ListItem key={filename} disablePadding>
-                    <Button
-                      variant={selectedFile === filename ? 'contained' : 'outlined'}
-                      onClick={() => handleFileClick(filename)}
-                      fullWidth
-                    >
-                      {quizStartTs && new Date(quizStartTs).toLocaleDateString()}
-                    </Button>
-                  </ListItem>
-                ))}
-            </List>
-          </>
-        )}
-        {quizContent && (
-          <Paper sx={{ mt: 2, p: 2 }}>
-            <Typography variant="h6" sx={{ mb: 2 }}>
-              Problems
-            </Typography>
-            <Divider sx={{ mb: 2 }} />
-            {quizContent.problems &&
-            typeof quizContent.problems === 'object' &&
-            Object.keys(quizContent.problems).length > 0 ? (
-              Object.entries(quizContent.problems).map(([pid, html]: [string, string]) => (
-                <Box key={pid} sx={{ mb: 2 }}>
-                  <Typography variant="subtitle1" sx={{ mb: 1 }}>
-                    Problem ID: {pid}
-                  </Typography>
-                  <div dangerouslySetInnerHTML={{ __html: html }} />
-                  <Divider sx={{ mt: 2 }} />
-                </Box>
-              ))
-            ) : (
-              <Typography color="text.secondary">No problems found in this quiz file.</Typography>
+            {selectedSemester && (
+              <>
+                <Typography variant="h6" gutterBottom>
+                  Course IDs
+                </Typography>
+                <List>
+                  {courseIds.map((cid) => (
+                    <ListItem key={cid} disablePadding>
+                      <Button
+                        variant={selectedCourseId === cid ? 'contained' : 'outlined'}
+                        onClick={() => setSelectedCourseId(cid)}
+                        fullWidth
+                      >
+                        {cid}
+                      </Button>
+                    </ListItem>
+                  ))}
+                </List>
+              </>
             )}
-          </Paper>
-        )}
+
+            {selectedCourseId && (
+              <>
+                <Typography variant="h6" gutterBottom>
+                  Quiz Files for <b>{selectedCourseId}</b>
+                </Typography>
+                <List>
+                  {[...courseFileInfos]
+                    .sort((a, b) => b?.quizStartTs - a.quizStartTs)
+                    .map(({ filename, quizStartTs }) => (
+                      <ListItem key={filename} disablePadding>
+                        <Button
+                          variant={selectedFile === filename ? 'contained' : 'outlined'}
+                          onClick={() => handleFileClick(filename)}
+                          fullWidth
+                        >
+                          {quizStartTs && new Date(quizStartTs).toLocaleDateString()}
+                        </Button>
+                      </ListItem>
+                    ))}
+                </List>
+              </>
+            )}
+          </Box>
+          <Box sx={{ flex: 1 }}>
+            {quizContent && (
+              <Paper sx={{ p: 2 }}>
+                <Typography variant="h6" sx={{ mb: 2 }}>
+                  Problems
+                </Typography>
+                <Divider sx={{ mb: 2 }} />
+                {quizContent.problems &&
+                typeof quizContent.problems === 'object' &&
+                Object.keys(quizContent.problems).length > 0 ? (
+                  Object.entries(quizContent.problems).map(([pid, html]: [string, string]) => (
+                    <Box key={pid} sx={{ mb: 2 }}>
+                      <Typography variant="subtitle1" sx={{ mb: 1 }}>
+                        Problem ID: {pid}
+                      </Typography>
+                      <div dangerouslySetInnerHTML={{ __html: html }} />
+                      <Divider sx={{ mt: 2 }} />
+                    </Box>
+                  ))
+                ) : (
+                  <Typography color="text.secondary">
+                    No problems found in this quiz file.
+                  </Typography>
+                )}
+              </Paper>
+            )}
+          </Box>
+        </Box>
       </Box>
     </MainLayout>
   );
