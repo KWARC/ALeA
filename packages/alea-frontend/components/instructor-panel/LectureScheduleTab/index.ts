87397eedc52ba98f7e2e6aafe7be2a1ba2743f7e
Shubham Chaurasia <82652474+shubh1118@users.noreply.github.com>
2025-11-10 10:55:20 +0530

    deleted divided components

--- packages/alea-frontend/components/instructor-panel/LectureScheduleTab/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as LectureScheduleTab } from './LectureScheduleTab';
-export { default as LectureScheduleSection } from './LectureScheduleSection';
-export * from './LectureScheduleTab';
