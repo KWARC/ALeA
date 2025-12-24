b1176a0b4656edc98b1f29cfc9f61549ac37f0b4
abchugh <achugh89@gmail.com>
2025-12-24 12:34:18 +0530

    Alter institutionId column to match comments_database_setup.sql

--- sql/temp_alter.sql
+++ sql/temp_alter.sql
@@ -2,16 +2,16 @@ alter table announcement
 add column institutionId varchar(255) not null default 'FAU' after instructorId;
 
 alter table comments
-add column institutionId varchar(255) not null default 'FAU' after courseTerm;
+add column institutionId varchar(255) null default 'FAU' after courseTerm;
 
 alter table homework
-add column institutionId varchar(255) not null default 'FAU' after courseInstance;
+add column institutionId varchar(255) null default 'FAU' after courseInstance;
 
 alter table homeworkHistory
-add column institutionId varchar(255) not null default 'FAU' after courseInstance;
+add column institutionId varchar(255) null default 'FAU' after courseInstance;
 
 alter table Answer
-add column institutionId varchar(255) not null default 'FAU' after courseInstance;
+add column institutionId varchar(255) null default 'FAU' after courseInstance;
 
 alter table excused
 add column institutionId varchar(255) not null default 'FAU' after courseInstance;
