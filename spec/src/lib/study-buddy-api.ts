81e3ba922099d4fa36db38e04b72671a8b4df9f3
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2026-01-12 09:32:08 +0530

    Unique dev (#842)
    * added insitituionId in  tables and refactor api
    
    * Update comments_database_setup.sql
    
    * studdy buddy changes
    
    * changes
    
    * Update index.tsx
    
    * res message update
    
    ---------
    
    Co-authored-by: abchugh <achugh89@gmail.com>

--- spec/src/lib/study-buddy-api.ts
+++ spec/src/lib/study-buddy-api.ts
@@ -9,10 +9,11 @@ import {
   UserStats,
 } from './study-buddy';
 
-export async function getStudyBuddyUserInfo(courseId: string) {
+export async function getStudyBuddyUserInfo(courseId: string, institutionId: string, instanceId: string) {
   try {
     const resp = await axios.get(`/api/study-buddy/get-user-info/${courseId}`, {
       headers: getAuthHeaders(),
+      params: { institutionId, instanceId },
     });
     return resp.data as StudyBuddy;
   } catch (err) {
@@ -24,40 +25,42 @@ export async function getStudyBuddyUserInfo(courseId: string) {
   }
 }
 
-export async function updateStudyBuddyInfo(courseId: string, data: StudyBuddy) {
+export async function updateStudyBuddyInfo(courseId: string, data: StudyBuddy, institutionId: string, instanceId: string) {
   await axios.post(`/api/study-buddy/update-info/${courseId}`, data, {
     headers: getAuthHeaders(),
+    params: { institutionId, instanceId },
   });
 }
 
-export async function getStudyBuddyList(courseId: string) {
+export async function getStudyBuddyList(courseId: string, institutionId: string, instanceId: string) {
   const resp = await axios.get(`/api/study-buddy/get-study-buddies/${courseId}`, {
     headers: getAuthHeaders(),
+    params: { institutionId, instanceId },
   });
   return resp.data as GetStudyBuddiesResponse;
 }
 
-export async function setActive(courseId: string, active: boolean) {
+export async function setActive(courseId: string, active: boolean, institutionId: string, instanceId: string) {
   await axios.post(
     `/api/study-buddy/set-active/${courseId}`,
     { active },
-    { headers: getAuthHeaders() }
+    { headers: getAuthHeaders(), params: { institutionId, instanceId } }
   );
 }
 
-export async function connectionRequest(courseId: string, receiverId: string) {
+export async function connectionRequest(courseId: string, receiverId: string, institutionId: string, instanceId: string) {
   await axios.post(
     `/api/study-buddy/connection-request/${courseId}`,
     { receiverId },
-    { headers: getAuthHeaders() }
+    { headers: getAuthHeaders(), params: { institutionId, instanceId } }
   );
 }
 
-export async function removeConnectionRequest(courseId: string, receiverId: string) {
+export async function removeConnectionRequest(courseId: string, receiverId: string, institutionId: string, instanceId: string) {
   await axios.post(
     `/api/study-buddy/remove-connection-request/${courseId}`,
     { receiverId },
-    { headers: getAuthHeaders() }
+    { headers: getAuthHeaders(), params: { institutionId, instanceId } }
   );
 }
 
@@ -66,32 +69,33 @@ export async function purgeStudyBuddyData() {
   return resp.data as StudyBuddy;
 }
 
-export async function getStudyBuddyUsersStats(courseId: string, instanceId?: string) {
+export async function getStudyBuddyUsersStats(courseId: string, instanceId: string | undefined, institutionId: string) {
   const resp = await axios.get(`/api/study-buddy/get-users-stats/${courseId}`, {
     headers: getAuthHeaders(),
-    params: { instanceId },
+    params: { instanceId, institutionId },
   });
   return resp.data as UserStats;
 }
 
-export async function getAllUsersStats(instanceId: string) {
+export async function getAllUsersStats(instanceId: string, institutionId: string) {
   const resp = await axios.get<AllCoursesStats>('/api/study-buddy/get-all-users-stats', {
     headers: getAuthHeaders(),
-    params: { instanceId },
+    params: { instanceId, institutionId },
   });
   return resp.data;
 }
 
-export async function getStudyBuddyCoursesSortedbyConnections(instanceId: string) {
+export async function getStudyBuddyCoursesSortedbyConnections(instanceId: string, institutionId: string) {
   const resp = await axios.get<GetSortedCoursesByConnectionsResponse[]>(
     '/api/study-buddy/get-courses-sortedby-connections',
-    { headers: getAuthHeaders(), params: { instanceId } }
+    { headers: getAuthHeaders(), params: { instanceId, institutionId } }
   );
   return resp.data;
 }
-export async function getEnrolledCourseIds() {
+export async function getEnrolledCourseIds(institutionId: string, instanceId: string) {
   const resp = await axios.get(`api/study-buddy/get-enrolled-course-ids`, {
     headers: getAuthHeaders(),
+    params: { institutionId, instanceId },
   });
   return resp.data as EnrolledCourseIds[];
 }
