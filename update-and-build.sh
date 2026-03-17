4c784dbf7f5a3a887a022f0f7d76105727cafbe5
Abhishek Kumar <abhishek2021005@gmail.com>
2026-03-17 17:05:12 +0530

    prisma generate command while building in prod

--- update-and-build.sh
+++ update-and-build.sh
@@ -3,4 +3,5 @@ set -e
 
 git pull
 NX_DAEMON=false npx nx reset
+NX_DAEMON=false npx prisma generate --schema=prisma/comments/schema.prisma
 NX_DAEMON=false npx nx build alea-frontend --outputPath=dist/packages/alea-frontend-new
