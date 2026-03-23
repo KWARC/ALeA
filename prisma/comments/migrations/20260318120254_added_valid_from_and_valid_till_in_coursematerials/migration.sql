203dd3ca9d9b45b09213dbc6765ad479776e4587
abchugh <achugh89@gmail.com>
2026-03-23 13:44:50 +0530

    Update migration.sql

--- prisma/comments/migrations/20260318120254_added_valid_from_and_valid_till_in_coursematerials/migration.sql
+++ prisma/comments/migrations/20260318120254_added_valid_from_and_valid_till_in_coursematerials/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `coursematerials` ADD COLUMN `validFrom` VARCHAR(255) NULL,
+ALTER TABLE `CourseMaterials` ADD COLUMN `validFrom` VARCHAR(255) NULL,
     ADD COLUMN `validTill` VARCHAR(255) NULL;
